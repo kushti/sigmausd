7d5780de70afd2a0a459141c197ecb2a6cdbc461
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2024-03-04 23:57:41 +0300

    reading contracts stub

--- src/main/scala/sigmausd/SigmaUSDBootstrapping.scala
+++ src/main/scala/sigmausd/SigmaUSDBootstrapping.scala
@@ -1,5 +1,19 @@
 package sigmausd
 
+import sigmausd.ScriptUtil.{getAddressFromErgoTree, getStringFromAddress}
+import org.ergoplatform.ErgoAddressEncoder.TestnetNetworkPrefix
+
 object SigmaUSDBootstrapping extends ContractUtils with App {
   override def defaultSubstitutionMap: Map[String, String] = Map.empty
+
+  val networkPrefix = TestnetNetworkPrefix
+
+  val bankV1Script = readContract("contracts/v1/bank.es")
+  val bankV1ErgoTree = ScriptUtil.compile(Map(), bankV1Script)
+  val bankV1Address = getStringFromAddress(getAddressFromErgoTree(bankV1ErgoTree))
+
+  val bankV2Script = readContract("contracts/v1/bank.es")
+
+  val updateScript = readContract("contracts/v1/update.es")
+  val ballotScript = readContract("contracts/v1/ballot.es")
 }
