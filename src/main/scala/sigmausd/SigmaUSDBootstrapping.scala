0858c2d703c2d91e41fc003f696591bbe35a927c
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2024-03-05 00:46:57 +0300

    all the scripts

--- src/main/scala/sigmausd/SigmaUSDBootstrapping.scala
+++ src/main/scala/sigmausd/SigmaUSDBootstrapping.scala
@@ -12,8 +12,15 @@ object SigmaUSDBootstrapping extends ContractUtils with App {
   val bankV1ErgoTree = ScriptUtil.compile(Map(), bankV1Script)
   val bankV1Address = getStringFromAddress(getAddressFromErgoTree(bankV1ErgoTree))
 
-  val bankV2Script = readContract("contracts/v1/bank.es")
+  val bankV2Script = readContract("contracts/v2/bank.es")
+  val bankV2ErgoTree = ScriptUtil.compile(Map(), bankV2Script)
+  val bankV2Address = getStringFromAddress(getAddressFromErgoTree(bankV2ErgoTree))
 
   val updateScript = readContract("contracts/v1/update.es")
+  val updateErgoTree = ScriptUtil.compile(Map(), updateScript)
+  val updateAddress = getStringFromAddress(getAddressFromErgoTree(updateErgoTree))
+
   val ballotScript = readContract("contracts/v1/ballot.es")
+  val ballotErgoTree = ScriptUtil.compile(Map(), ballotScript)
+  val ballotAddress = getStringFromAddress(getAddressFromErgoTree(ballotErgoTree))
 }
